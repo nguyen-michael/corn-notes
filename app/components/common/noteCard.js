845dcf76652646a31e35d9387cd56f6ad5ac7a1b
Tim LaRou <larou.tim@gmail.com>
2017-06-24 10:38:55 -0500

    NoteCard Comp

---
+++ app/components/common/noteCard.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+
+// this should work?
+export default (props) => {
+
+
+  return(
+
+   <div className="col s6 m3 noteCard">
+
+                <div className="card small">
+                    <div className="card-image">
+                        <img src={this.prop.cardImage} />
+                        <span className="card-title text-shadow">{props.header} - {props.subheader}</span>
+                    </div>
+                    <div className="card-content">
+                        <p>{props.summary}</p>
+                    </div>
+                    <div className="card-action">
+                        <a href={props.link}>See Note</a>
+                    </div>
+                </div>
+
+            </div>
+  );
+};
+
+
+
+
