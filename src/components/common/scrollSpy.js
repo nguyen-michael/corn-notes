5012d633f259b24891bc614a2a0183c81a3afa05
unknown <nguyen.michl@gmail.com>
2017-07-08 02:23:11 -0500

    log out button functionality

--- src/components/common/scrollSpy.js
+++ src/components/common/scrollSpy.js
@@ -3,6 +3,11 @@ import ListElement from "./scrollSpyListElement";
 
 //can decide to pass in list elements to dynamically generate the side nav
 class ScrollSpy extends Component {
+    constructor(props) {
+        super(props);
+
+        this.logout = this.logout.bind(this);
+    }
 
 
     renderList() {
@@ -15,13 +20,18 @@ class ScrollSpy extends Component {
         ));
     }
 
+    logout() {
+        this.props.auth.logout();
+    }
+
     render() {
         return (
 
             <div className="col hide-on-small-only m3 l2">
                 <div className="toc-wrapper pinned" style={styles.wrapperStyle}>
                     <a href="/allnotes" className="btn"><i className="material-icons">menu</i></a>
-                    <a  className="btn" ><i className="material-icons">settings_power</i></a>
+                    {/* Logout Button */}
+                    <a  className="btn" onClick={this.logout}><i className="material-icons">settings_power</i></a>
 
                     <div style={styles.listDivStyle}>
 
